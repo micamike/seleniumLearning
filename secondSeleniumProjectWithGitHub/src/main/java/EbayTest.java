5721973678b5a9446f119eca71f6807f3f8eee87
micamike <micamike@hotmail.com>
2018-08-26 15:33:56 +0300

    Added

--- secondSeleniumProjectWithGitHub/src/main/java/EbayTest.java
+++ secondSeleniumProjectWithGitHub/src/main/java/EbayTest.java
@@ -14,18 +14,55 @@ public class EbayTest {
 
             driver.get("https://www.ebay.com/");
 
+            driver.findElementByLinkText("register").click();
 
-            driver.quit();
+            //Fill firstname field while registration
+            fillFirstNameForm();
+
+
+            //Fill lastname field while registration
+            fillLastNameForm();
+
+            fillEmail();
+
+            fillPassword();
+
+            //Click on button
+            clickOnButtonSubReg();
+
+            // driver.quit();
 
 
 
         }
 
+    public void clickOnButtonSubReg() {
+        driver.findElementById("ppaFormSbtBtn").click();
+    }
 
+    public void fillPassword() {
+        driver.findElementById("PASSWORD").click();
+        driver.findElementById("PASSWORD").clear();
+        driver.findElementById("PASSWORD").sendKeys("telran55");
+    }
 
+    public void fillEmail() {
+        driver.findElementById("email").click();
+        driver.findElementById("email").clear();
+        driver.findElementById("email").sendKeys("testMickaelQA+" + TimeUnit.SECONDS +"@hotmail.com");
+    }
 
+    public void fillLastNameForm() {
+        driver.findElementByName("lastname").click();
+        driver.findElementByName("lastname").clear();
+        driver.findElementByName("lastname").sendKeys("Test");
+    }
 
-
+    public void fillFirstNameForm() {
+        driver.findElementByName("firstname").click();
+        driver.findElementByName("firstname").clear();
+        driver.findElementByName("firstname").sendKeys("Mickael");
+    }
 
 
 }
